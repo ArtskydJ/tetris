3fa4945a9cbd7449f0fdb7836d30cac0e8151131
Joseph Dykstra <josephdykstra@gmail.com>
2015-09-18 07:16:37 -0500

    tetris3

--- Tetris2.c
+++ Tetris2.c
@@ -9,7 +9,12 @@
 #include <string.h>
 
 #define DEBUG
-#define MODE  2
+#define MODE	0
+//#define ONE_FLOOR_KICK
+#define SHOW_NEXT_PIECES
+#define SHOW_GHOST_PIECE
+#define SHOW_CURR_PIECE
+
 
 #include "DefinitionsAndData.c"
 #include "SmallFunctions.c"
@@ -18,93 +23,109 @@
 #include "InputOutputAndDraw.c"
 #ifdef DEBUG
 	#include "Debug.c"
+	#ifdef MODE
+		#undef MODE
+	#endif
+	#define MODE	0
+//	#define DEBUG_STREAM		//turns on most debug stream messages
+//	#define DEBUG_BOARD			//turns on board drawing in debug stream
+//	#define DEBUG_INFO			//turns on debug stream info about game's state
+//	#define DRAW_TET_DETAILS	//turns on details of tetromino drawing positions
 #endif
 
 
-/*Modes:
- *0	normal
- *1	fast
- *2	debug clearFullRows()
- *3	dubug rotation
- *4	debug peiceSetinto()
- *5	debug peiceCreate()
- *6	debug gravity in inputOutput()
- */
+/* ************************************ *\
+|*		Modes:							*|
+|* 0	Normal							*|
+|* 1	Fast							*|
+|* 2	Debug clearFullRows()			*|
+|* 3	Dubug rotation					*|
+|* 4	Debug peiceSetinto()			*|
+|* 5	Debug peiceCreate()				*|
+|* 6	Debug gravity in inputOutput()	*|
+\* ************************************ */
 
 
 int main( int argc, char *argv[] )
 	{
-	//SDL_Event event;
-	//int quit = 0;
 	initReset();
 	srand(time(0));
-	ClearScreen();
 	if (MODE==0||MODE==1)
 		{
-		char playagain=true;
-		char piecemoving;
 		while (playagain)
 			{
 			ClearScreen();
-			selectGame(1);
+			selectGame(1); //selectGame(MODE); //don't have way to select game types yet.
 			gameResetVars();
-			selectGame(MODE);
 			alive=true;
 			while (alive)
 				{
-				pieceResetVars();
 				pieceCreate();
-				piecemoving=move(dwn,tetUD);	//If a piece was made where it...
-				alive=piecemoving;				//can't move down, end the game.
-				if (alive) ClearScreen();
-				for (int i=0; piecemoving; i++) //waiting
+				alive = move(dwn,tetUD);	//If a piece was made where it...
+				piecemoving = alive;			//can't move down, end the game.
+				for (int i=1; piecemoving; i++) //while the piece is moving
 					{
-					piecemoving=inputOutput(true,true,i);
-					boardDraw(true,(i%10)==0);
-					SDL_Delay(10);
-#ifdef DEBUG
-					debugStream(i);
+					i%=waitGravity;
+					drawBoard(true); //replace [false] with [i==0]
+#ifdef DEBUG_INFO
+					debugInfo(i);
 #endif
+					SDL_Delay(delaytime/waitGravity); // was 10
+					piecemoving=inputOutput(true,true,i);
 					}
-				delaytime*=speedup;
+				//delaytime*=speedup; //needs float typecast?
 				pieceSetInto(currentPiece);
 				clearFullRows();
 				}
 			playagain=gameEnd();
 			}
 		}
+#ifdef DEBUG
 	else
 		{
 		debug(MODE);
 		}
+#endif
 	SDL_Quit();
 	return 0;
 	}
 
+	
+	
+	
+	
 /*
 BUGS
-Mode 0:
-	Unfunctional last checked.
 
-Mode 1:
-	Unfunctional last checked.
+FIX!
 
-Debug Mode 2:
-	stdout.txt looks great, but 2nd print does not match.
-	bug in printing?
+FIXED?
+pieces rotate into others (off by 2 because of hidden 2 rows?)
+hard drop acts like firm drop
+	
+	
+TODO
++ use the Random Generator
++ make a hold piece
++ add bonus points for combos
++ add piece, line, score, and level counters.
++ rotate the nextPiece so that WYSIWYG
++ create pause functionality
++ clean up the code...
+	- put all [printf] functions between [#ifdef DEBUG] statements
+	- take out unused functions
+	- name all functions with camelCase
+	- name all variables with camelCase
+	- name all #defines in ALL_CAPS
+	- change all [UD] and [LR] to [y] and [x]
+	- replace clearfullrows() [if (INallowLog)] with [#ifdef DEBUG] statements
+	- make some variables local?
+	- deside which checks for the end of game:
+		[piecemoving=move(dwn,tetUD);]	// Tetris3.c			r:55	c:17
+		[if (blocksInRow>0 && i<2)]		// TetrominoManip.c		r:171	c:9
 
-Debug Mode 3:
-	works great last checked!
-
-Debug Mode 4:
-	not checked yet.
-
-Debug Mode 5:
-	not checked yet.
-
-Debug Mode 6:
-	not checked yet.
-
+DONE
++ clean up the debug stream; too much un-needed junk in it
 
 
 
